ba2dc9dd8aba707bf7d1bc5c8335fb5387fc401b
IgnisDa <ignisda2002@gmail.com>
2020-12-16 15:12:55 +0530

    feat(inscribe/settings.py): add home, django_extensions apps

--- inscribe/settings.py
+++ inscribe/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # user installed apps
+    'home.apps.HomeConfig',
+
+    # django add-ons
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
