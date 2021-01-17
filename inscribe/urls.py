92b45ff1431ea1e2278c51293c6461a1276752d0
IgnisDa <ignisda2001@gmail.com>
2021-01-17 23:02:46 +0530

    Merge branch 'development' and helptext changes

--- inscribe/urls.py
+++ inscribe/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
+    path("notes/", include("notes.urls")),
     path("accounts/", include("accounts.urls")),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
 
-# Create your views here.
+from . import models
+
+
+# The below view should be a `CreateView` but since the model isn't created, we
+# will be using a `TemplateView` instead
+class NoteCreateView(TemplateView):
+    """ This view will handle the creation of notes, and saving them to the database """
+
+    # model = models.Note
+    template_name = "notes/create.html"
